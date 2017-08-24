14464b29668eca80ca321ef4058f28ffa68270bf
Felipe Miranda <felipe.miranda.mdrs@gmail.com>
2017-08-24 16:47:59 -0300

    Add module service info

--- samples/test.js
+++ samples/test.js
@@ -6,24 +6,4 @@ setInterval(() => {
   }).catch((error) => {
     console.log(error);
   });
-}, 700);
-
-
-setInterval(() => {
-  plugger.exec('world__ping').then((value) => {
-    console.log(value);
-  });
-}, 300);
-
-/*setInterval(() => {
-  plugger.exec('primo__1.0.0__ehPrimo', 7568).then((value) => {
-    console.log(value);
-  });
-}, 300);*/
-/*
-setInterval(() => {
-  plugger.exec('primo__2.0.0__ehPrimo', 144547).then((value) => {
-    console.log(value);
-  });
-}, 300);
-*/
+}, 100);
